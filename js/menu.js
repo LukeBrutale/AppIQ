548714b8ba5ca3fe99b34475b4f4c2341ca7adaa
LukeBrutale <yuriybema@gmail.com>
2023-04-20 15:01:28 +0300

    commit

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,9 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
-    const mobileBtnClose = document.querySelector("[data-menu-close]");
+  const mobileBtnClose = document.querySelector("[data-menu-close]");
+  
+  
   
     menuBtnRef.addEventListener("click", () => {
       mobileMenuRef.classList.toggle("is-open");
